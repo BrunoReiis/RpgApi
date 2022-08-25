64250f3fce7f2a449e7e15ca408680b5db161978
Bruno <bruno.reiis15@gmail.com>
2022-08-25 08:56:29 -0300

    Criacao

---
+++ Controllers/PersonagensController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RpgApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class PersonagensController : ControllerBase
+    {
+        
+    }
+}
